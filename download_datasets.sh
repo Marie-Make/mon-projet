fa4ea8f6e57d40177110ef9c9dc5626d94db9635
Marie-Make <75613302+Marie-Make@users.noreply.github.com>
2021-02-08 10:52:25 +0100

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -5,7 +5,7 @@ if [[ $FILE != "WFG2FG" ]]; then
     exit 1
 fi
 mkdir ./input
-URL=https://drive.google.com/drive/my-drive/WFG2FG.rar
+URL=https://drive.google.com/drive/my-drive/WFG2FG.zip
 ZIP_FILE=./input/$FILE.rar
 TARGET_DIR=./input/$FILE/
 wget -N $URL -O $ZIP_FILE
